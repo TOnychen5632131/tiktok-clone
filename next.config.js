c097337b85f4e5b46471fe141214fea438500331
TOnychen5632131 <162278003+TOnychen5632131@users.noreply.github.com>
2025-01-20 09:28:49 +0800

    production

--- next.config.js
+++ next.config.js
@@ -7,7 +7,7 @@ const withPWA = require('next-pwa')({
   cacheStartUrl: false,
   dynamicStartUrl: true,
   runtimeCaching: [],
-  disable: process.env.NODE_ENV === 'development',
+  disable: process.env.NODE_ENV === 'production',
 });
 
 const nextConfig = withPWA({
